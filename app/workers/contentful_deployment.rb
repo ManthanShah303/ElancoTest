d11c70e3daaa43af7a0e6f30536706b2e49272e2
ashutosh8 <C217636@VM-UDGCOEXL-008.AM.LILLY.com>
2016-06-20 21:01:11 +0530

    updated contentful deployment and migration scripts

--- app/workers/contentful_deployment.rb
+++ app/workers/contentful_deployment.rb
@@ -12,60 +12,84 @@ module ContentfulDeployment
                     ENV['CONTENTFUL_MANAGEMENT_TOKEN'],
                     default_locale: 'en-GB',
                     proxy_host: ENV['PROXY'].nil? ? nil : ENV['PROXY'].split(':')[0],
-                    proxy_port: ENV['PROXY'].nil? ? nil : ENV['PROXY'].split(':')[1].to_i,
-                    raise_errors: true
+                    proxy_port: ENV['PROXY'].nil? ? nil : ENV['PROXY'].split(':')[1].to_i             
                   )
             
-            space = client.spaces.find(ENV['CONTENTFUL_SPACE_ID'])
-            return "Error initializing Contentful space #{space.error}" if space.try(:error)
+            @space = client.spaces.find(ENV['CONTENTFUL_SPACE_ID'])
+            return "Error initializing Contentful space #{@space.error}" if @space.try(:error)
            
-            create_content_types(space)
+            create_content_types
             
-            puts ">>>>>>>>>> Asset Deployment Complete"
+            puts ">>>>>>>>>> Contentful Deployment Complete"
             
-            #ContentfulEntryMigration.perform
+            ContentfulEntryMigration.perform
             :success
             
     end
-    
-    def self.create_content_types(space)
-      if space.content_types.find('json-configuration-object').try(:error)
+
+    # Ensure the json-configuration-object content type is been created in the Conentful space that is being deployed to
+    # This method is meant as a convienience when deploying to a new space
+    # It will create the content types and entries which are known to be needed when setting up a new space
+    def self.create_content_types
+      if @space.content_types.find('json-configuration-object').try(:error)
         puts "Creating JSON Configuration Object Content Type...."
        
-        jsonconfigobj = space.content_types.create(id: 'json-configuration-object', name: 'JSON Configuration Object')
-       
-        
+        jsonconfigobj = @space.content_types.create(id: 'json-configuration-object', name: 'JSON Configuration Object')
+                
         jsonconfigobj.fields.create(id: 'name', name: 'Name', type: 'Symbol', localized: false)
         jsonconfigobj.fields.create(id: 'value', name: 'Value', type: 'Object', localized: true)
+
+        return "Error creating Contentful content type #{@content_type.error}" if @content_type.try(:error)       
+
         jsonconfigobj.update(displayField: 'name')
         jsonconfigobj.activate
 
         puts "Creating Entry/Entries which is/are known for JSON Configuration Object"
-        contents = []
-        contents << jsonconfigobj.entries.create(id: 'index.json', name: 'index.json')
-        contents << jsonconfigobj.entries.create(id: 'contact.json', name: 'contact.json')
-        contents.map(&:publish)
+        
+        jsonconfigobj.entries.create(id: 'index.json', name: 'index.json')
+        jsonconfigobj.entries.create(id: 'contact.json', name: 'contact.json')
       end
     end
     
+
     def self.create_entry(space_id, content_type_id, entry_id)
         client = Contentful::Management::Client.new(
                    ENV['CONTENTFUL_MANAGEMENT_TOKEN'],
                    default_locale: 'en-GB',
                    proxy_host: ENV['PROXY'].nil? ? nil : ENV['PROXY'].split(':')[0],
-                   proxy_port: ENV['PROXY'].nil? ? nil : ENV['PROXY'].split(':')[1].to_i,
-                   raise_errors: true
+                   proxy_port: ENV['PROXY'].nil? ? nil : ENV['PROXY'].split(':')[1].to_i                  
             )
             
-        space = client.spaces.find(space_id.to_s)
-        return "Error initializing Contentful space #{space.error}" if space.try(:error)
+        @space = client.spaces.find(space_id.to_s)
+        return "Error initializing Contentful space #{@space.error}" if @space.try(:error)
          
-        content_type = space.content_types.find(content_type_id.to_s)
-        return "Error initializing Contentful content type #{content_type.error}" if content_type.try(:error)
+        @content_type = @space.content_types.find(content_type_id.to_s)
+        return "Error initializing Contentful content type #{@content_type.error}" if @content_type.try(:error)
        
-        puts "Creating Entry for #{content_type.name}"
+        puts "Creating Entry for #{@content_type.name}"
         
-        entry = content_type.entries.create(id: entry_id.to_s)
-        entry.publish
+        @entry = @content_type.entries.create(id: entry_id.to_s)
+        return "Error creating Contentful entry  #{@entry.error}" if @entry.try(:error)
+
+        :success
+    end
+
+    def self.create_asset(asset_identity)
+        # set up the basic HTTP request, using proxy if configured, which will be used to push entries to the destination space
+        request = ContentfulEntryMigration.proxy
+                    .auth("Bearer #{ENV['CONTENTFUL_MANAGEMENT_TOKEN']}")
+                    .headers("Content-Type" => "application/vnd.contentful.management.v1+json")
+
+        asset_id = asset_identity.to_s
+        current_entry_version = 1
+        puts "Creating asset to contentful space #{ENV['CONTENTFUL_SPACE_ID']}\n"
+
+        res = request.headers("X-Contentful-Version" => current_entry_version)
+                     .put("https://api.contentful.com/spaces/#{ENV['CONTENTFUL_SPACE_ID']}/assets/#{asset_id}")
+
+        puts res
+        return res.body.inspect if res.code >= 400
+
+        puts "Asset created successfully!"
     end
 end
